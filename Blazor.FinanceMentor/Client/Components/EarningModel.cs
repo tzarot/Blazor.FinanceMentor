5766b0088c936a2a6d876e135268277759bdf549
Themistoklis Zarotiadis <Themistoklis.Zarotiadis@arhsdev.onmicrosoft.com>
2022-03-22 21:57:56 +0200

    Added Data Annotation attributes and Validation Components

--- Blazor.FinanceMentor/Client/Components/EarningModel.cs
+++ Blazor.FinanceMentor/Client/Components/EarningModel.cs
@@ -1,15 +1,21 @@
-﻿using Blazor.FinanceMentor.Shared;
+﻿using System.ComponentModel.DataAnnotations;
+using Blazor.FinanceMentor.Shared;
 
 namespace Blazor.FinanceMentor.Client.Components
 {
     public class EarningModel
     {
+        [Required]
         public DateTime Date { get; set; }
 
+        [Required]
+        [StringLength(50)]
         public string Subject { get; set; }
-        
+
+        [Required]
         public EarningCategory Category { get; set; }
-        
+
+        [Required]
         public decimal Amount { get; set; }
     }
 }
