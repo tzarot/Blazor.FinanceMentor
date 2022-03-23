83182caf098bf62393859ced9deec1d814e6a362
Themistoklis Zarotiadis <Themistoklis.Zarotiadis@arhsdev.onmicrosoft.com>
2022-03-23 18:50:40 +0200

    Impemented Client for Expenses

---
+++ Blazor.FinanceMentor/Client/Components/ExpenseModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Blazor.FinanceMentor.Shared;
+
+namespace Blazor.FinanceMentor.Client.Components
+{
+    public class ExpenseModel
+    {
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Subject { get; set; }
+
+        [Required]
+        public ExpenseCategory Category { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+    }
+}
