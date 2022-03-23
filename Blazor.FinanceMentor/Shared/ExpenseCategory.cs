1bef05f0aed8026ec613343db95cf1483a837a78
Themistoklis Zarotiadis <Themistoklis.Zarotiadis@arhsdev.onmicrosoft.com>
2022-03-23 18:28:38 +0200

    Added Expense and ExpenseCategory

---
+++ Blazor.FinanceMentor/Shared/ExpenseCategory.cs
@@ -0,0 +1,20 @@
+﻿namespace Blazor.FinanceMentor.Shared;
+
+public enum ExpenseCategory
+{
+    Housing,
+    Transportation,
+    Food,
+    Utilities,
+    Clothing,
+    HealthCare,
+    Personal,
+    Debt,
+    Retirement,
+    Education,
+    Savings,
+    Gifts,
+    Donations,
+    Entertainment
+
+}
