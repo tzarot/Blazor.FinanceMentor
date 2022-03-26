4b5bdbbc4216eb818bd2b62a502dfd70d2005115
Themistoklis Zarotiadis <Themistoklis.Zarotiadis@arhsdev.onmicrosoft.com>
2022-03-26 10:24:06 +0200

    Added SpecFlow project. Added id for NavMenu component in Client project to be discoverable. Added SpecFlow Feature/StepDefinition for testing correct Client App opening by retrieving NavMenu item

---
+++ Blazor.FinanceMentor.SpecFlow.Test/Features/BasicTests.feature
@@ -0,0 +1,7 @@
+﻿Feature: BasicTests
+
+Basic Test the App 
+
+@tag1
+Scenario: App successfully starts
+	Then the home page is loaded
---
+++ Blazor.FinanceMentor.SpecFlow.Test/StepDefinitions/BasicTestsStepDefinitions.cs
@@ -0,0 +1,37 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Blazor.FinanceMentor.SpecFlow.Test.StepDefinitions
+{
+    [Binding]
+    public sealed class BasicTestsStepDefinitions
+    {
+        private IWebDriver _driver { get; set; }
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            var url = "https://localhost:7275/";
+            _driver = new ChromeDriver(Environment.CurrentDirectory);
+            _driver.Navigate().GoToUrl(url);
+            _driver.Manage().Window.Maximize();
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            _driver.Quit();
+        }
+
+        [Then(@"the home page is loaded")]
+        public void ThenTheHomePageIsLoaded()
+        {
+            var navMenu = _driver.FindElement(By.Id("navmenufm"));
+            Assert.IsNotNull(navMenu);
+        }
+
+
+    }
+}
